e826f4ac3ed99e4c1d94f36b969da746ad16def4
yahya-ifiveo <yahya@ifiveo.com>
2024-05-20 17:40:47 +0500

    Make prediction on sample video using YOLOv8-Nano pre-trained model.

---
+++ main.py
@@ -0,0 +1,27 @@
+import cv2
+from ultralytics import YOLO
+
+## Load YOLOv8-Nano pre-trained model ##
+model = YOLO("yolov8n.pt")
+## Intialize Video Capture ##
+cap = cv2.VideoCapture("testing.mp4")
+## Check to make sure reading video ##
+assert cap.isOpened(), "Error reading video file"
+
+# While Loop to get frame-by-by frame from video
+while cap.isOpened():
+    success, frame = cap.read()  # Read frames
+    if not success:
+        print("Video frame is empty or video processing has been successfully completed.")
+        break
+    # Make model predictions on each frame for specific class_id = 0: person
+    tracks = model.track(frame, persist=True, show=False, classes = 0)
+
+    """Display frame."""
+    cv2.imshow("Frane", frame)
+    # Break Window
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
