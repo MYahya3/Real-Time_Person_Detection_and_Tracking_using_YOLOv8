1101b37e512f6a9b3abb8ac5b56a478ea636c5bc
yahya-ifiveo <yahya@ifiveo.com>
2024-05-21 16:37:45 +0500

    Update main.py

--- main.py
+++ main.py
@@ -8,15 +8,18 @@ model = YOLO("yolov8n.pt")
 ## Intialize Video Capture ##
 try:
     rtsp_url = 'rtsp://192.168.100.4:8080/h264_ulaw.sdp'
-    # cap = cv2.VideoCapture(rtsp_url)
+    cap = cv2.VideoCapture(rtsp_url)
     # For Test Sample Vidoe
-    cap = cv2.VideoCapture("input_video/sample.mp4")
+    # cap = cv2.VideoCapture("input_video/sample.mp4")
 except:
     print("Retry to initiate your IP webcam")
     raise ConnectionError
 
 ## Check to make sure reading video ##
 assert cap.isOpened(), "Error reading video file"
+
+# Set the frame rate (fps)
+fps = cap.get(cv2.CAP_PROP_FPS)
 
 # Initialize Tracker Class
 Tracker = TrackProcessor()
@@ -26,8 +29,9 @@ class_id = 0  ### Class_id 0 is for Person detection only
 # Set the mouse callback function for the window
 cv2.namedWindow('Frame')
 cv2.setMouseCallback('Frame', Tracker.mouse_callback, {'frame_count': 5})
-frame_count = 0
+Tracker.frame_rate = fps
 
+frame_count = 0
 # While Loop to get frame-by-by frame from video
 while cap.isOpened():
     success, frame = cap.read()  # Read frames
